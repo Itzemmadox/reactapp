325ffc19e8d03fc163744173577ceb0c7a630938
Itzemmadox <kehindeoluwaseunemmanuel@gmail.com>
2024-02-02 23:27:17 +0100

    a clear initial structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Employee from './components/employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev')
+  const showEmployee = true
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {showEmployee ? 
+      <>
+        <input type='text' onChange={(e) => {
+          console.log(e.target.value);
+          setRole(e.target.value);
+        }} 
+
+      />
+        <Employee name="Seun" role="intern"/>
+        <Employee name="Emmanuel" role={role}/>
+        <Employee name="Seun" role="intern"/>
+     </> 
+     :(
+      <p>You cannot see the employees</p>
+     )
+    }
+       
     </div>
   );
 }
---
+++ src/components/employee.js
@@ -0,0 +1,11 @@
+function Employee(props){
+    return(
+        <>
+            <h3>Employee {props.name}</h3>
+            <p>{props.role ? props.role : "No role"}</p>
+            {/* {props.role ? <p class="role">{props.role}</p> : <p class="no-role">No role</p>} */}
+        </>
+    )
+}
+
+export default Employee
