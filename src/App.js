7c903fe19d912fdd0e45e68128d72a0c56af2dbc
Itzemmadox <kehindeoluwaseunemmanuel@gmail.com>
2024-02-08 10:25:20 +0100

    using component as props

--- src/App.js
+++ src/App.js
@@ -1,67 +1,107 @@
 import './index.css';
-import Employee from './components/employee';
+import Employee from './components/Employee';
 import { useState } from 'react';
-import {v4 as uuidv4} from 'uuid';
+import {v4 as uuidv4} from 'uuid'
+import AddEmployee from './components/AddEmployee';
+import EditEmployee from './components/EditEmployee';
+
+// import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev')
   const [employees, setEmployees] = useState(
     [
       {
+        id: 1,
         name: "Caleb", 
         role: "Devrel Engineer", 
-        image: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
+        img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
       },
       {
+        id: 2,
         name: "Sal", 
         role: "Manager", 
-        image: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"
+        img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"
       },
       {
+        id: 3,
         name: "John", 
         role: "Director of Eng.", 
-        image: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg"
+        img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg"
       },
       {
+        id: 4,
         name: "Melanie", 
         role: "Software Engineer", 
-        image: "https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg"
+        img: "https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg"
       },
       {
+        id: 5,
         name: "Corey", 
         role: "The Devops guy", 
-        image: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
+        img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
       },
       {
+        id: 6,
         name: "Jake", 
         role: "Senior Intern", 
-        image: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg"
+        img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg"
       }
     ]
-  )
+  );
+
+  function updateEmployee(id, newName, newRole){
+    const updatedEmployees = employees.map((employee) => {
+      if (id === employee.id) {
+        return {...employee, name: newName, role: newRole}
+      }
+      return employee
+    });
+    setEmployees(updatedEmployees)
+  }
+
+  function newEmployee(name, role, img) {
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+    }
+    setEmployees([...employees, newEmployee])
+  }
+
   const showEmployee = true
   return (
     <div className="App">
     {showEmployee ? 
       <>
         <input type='text' onChange={(e) => {
-          console.log(e.target.value);
           setRole(e.target.value);
         }} 
-
       />
       <div className='flex flex-wrap justify-center'>
         {employees.map((employee) => {
+          const editEmployee = ( 
+            <EditEmployee 
+              id={employee.id}
+              name={employee.name} 
+              role={employee.role} 
+              updateEmployee={updateEmployee}
+            />
+          );
           return(
             <Employee 
-                key={uuidv4()}
+                key={employee.id}
+                id={employee.id}
                 name={employee.name} 
                 role={employee.role} 
-                image={employee.image}
+                img={employee.img}
+                editEmployee={editEmployee}
             />
           );
         })}
       </div>
+      <AddEmployee newEmployee={newEmployee} />
      </> 
      :(
       <p>You cannot see the employees</p>
--- src/components/employee.js
+++ src/components/employee.js
@@ -1,20 +1,20 @@
-import EditEmployee from "../EditEmployee"
+import EditEmployee from "./EditEmployee"
 
 function Employee(props){
     return(
-        <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <img class="object-cover h-[100px] w-[100px] block mx-auto rounded-full sm:mx-0 sm:shrink-0" src={props.image} alt="" />
+        <div className="min-w-[350px] nax-w-[350px] m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+            <img className="object-cover h-[100px] w-[100px] block mx-auto rounded-full sm:mx-0 sm:shrink-0" src={props.img} alt="" />
             <div className="text-center space-y-2 sm:text-left">
-                <div class="space-y-0.5">
-                <p class="text-lg text-black font-semibold">
+                <div className="space-y-0.5">
+                <p className="text-lg text-black font-semibold">
                     {props.name}
                 </p>
-                <p class="text-slate-500 font-medium">
+                <p className="text-slate-500 font-medium">
                     {props.role}
                 </p>
                 </div>
 
-                <EditEmployee />
+                {props.editEmployee}
             </div>
         </div>
     )
